970641d9a2327d61021c5617f919c7d56a610933
Reza Norouzian <rnorouzian@gmail.com>
2022-03-08 22:58:56 -0600

    Update e.r

--- e.r
+++ e.r
@@ -1051,7 +1051,7 @@ return(fit)
 
 G_pca <- function(fit) {
   
-  if(!inherits(fit, c("lmerMod", "lmerModLmerTest", "lme4"))) stop("Non-lme4 model detected.", call. = FALSE)
+  if(!inherits(fit, c("lmerMod", "lmerModLmerTest", "lme4", "glmerMod"))) stop("Non-lme4 model detected.", call. = FALSE)
   obj <- summary(lme4::rePCA(fit))
   model <- lme4::VarCorr(fit)
   if(length(obj) == length(model)) {
